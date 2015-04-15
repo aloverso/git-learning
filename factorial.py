fb8dcefaffa3a8f0d8db04fbff7e54c92a89694f
lpsoares <lpsoares@gmail.com>
2015-04-15 15:46:10 -0300

    Putting tests in the program

--- factorial.py
+++ factorial.py
@@ -2,11 +2,22 @@
 """
 Created on Wed Apr 15 15:35:02 2015
 
-@author: aloverso
+@author: aloverso & Luciano
 """
 
+# Calculates the factorial of a number
 def factorial(n):
-    if n==1:
+    """
+    Testing the factorial
+    >>> factorial(3)
+    6
+    >>> factorial(5)
+    120
+    """
+    if n<=1:
         return n
     return n * factorial(n-1)
 
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
